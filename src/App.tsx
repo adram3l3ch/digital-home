77c450597784518e750f829520ca5f6cfd1fb174
Adarsh S <adramelech.psd@gmail.com>
2024-09-11 21:46:30 +0530

    add a tag rel

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,14 @@ const App = () => (
             <Router />
         </div>
         <div className="attribution">
-            Developed by <a href="https://github.com/adram3l3ch">adram3l3ch</a> & Designed by <a href="https://dribbble.com/haloweb">Halo UI UX</a>
+            Developed by{" "}
+            <a href="https://github.com/adram3l3ch" target="_blank" rel="noopener noreferrer">
+                adram3l3ch
+            </a>{" "}
+            & Designed by{" "}
+            <a href="https://dribbble.com/haloweb" target="_blank" rel="noopener noreferrer">
+                Halo UI UX
+            </a>
         </div>
     </div>
 );
