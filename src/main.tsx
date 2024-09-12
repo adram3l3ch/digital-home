8bc1050d379d4c0b26c29a82df1521511d5625ff
Adarsh S <adramelech.psd@gmail.com>
2024-09-12 18:46:53 +0530

    add base route

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { DeviceContextProvider } from "./context";
 const root = createRoot(document.getElementById("root")!);
 
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/digital-home">
         <DeviceContextProvider>
             <App />
         </DeviceContextProvider>
